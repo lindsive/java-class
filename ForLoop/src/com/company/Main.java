b4297c51055430c1dc0ea270c5da19d6debb1663
lindsive <ivesonlindsay@gmail.com>
2020-06-02 22:55:31 -0400

    added ForLoop exercise/lesson folder

---
+++ ForLoop/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int count = 0;
+        int sum = 0;
+
+        for (int i = 1; i <= 1000; i++) {
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                System.out.println(i + " can be divided by both 3 and 5");
+                count++;
+                sum = sum + i;
+            }
+            if (count == 5) {
+                break;
+            }
+        }
+        System.out.println(sum + " is the sum of all 3 numbers");
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,65 @@
 package com.company;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("challenge 1");
+        // using the for statement, call the calculateInterest method with
+        // the amount of 10000 w an interestRate of 2, 3, 4, 5, 6, 7, 8
+        // and print the results to the console
+
+        for(int i = 2; i < 9; i++) {
+            System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10000.0, i)));
+        }
+
+        System.out.println(" ");
+        System.out.println("challenge 2");
+        // how would you modify the for loop above to do the same thing
+        // as shown but to start from 8% and work back to 2%
+        for(int i = 8; i > 1; i--) {
+            System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10000.0, i)));
+        }
+
+        System.out.println(" ");
+        System.out.println("challenge 3");
+
+        int count = 0;
+        for (int i = 10; i < 50; i++) {
+            if (isPrime(i)) {
+                count++;
+                System.out.println(i + " is a prime number");
+                if (count == 10) {
+                    break;
+                }
+            }
+        }
+
+
+    }
+    public static double calculateInterest(double amount, double interestRate) {
+        return (amount * (interestRate/100));
+    }
+
+    // create a for statement using any range of numbers
+    // determine if the number is a prime number using the isPrime method
+    // if it is a prime number, print it out and increment a count of the
+    // number of the prime numbers found
+    // if the count is 3 exit the for loop
+    // hint: use the break; statement to exit
+
+
+    public static boolean isPrime(int n) {
+        if (n == 1) {
+            return false;
+        }
+        for (int i = 2; i <= n/2; i++) {
+            if(n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
