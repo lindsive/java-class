8fb91d82e4aeb685882e6dd2c66447efdc54122e
lindsive <ivesonlindsay@gmail.com>
2020-06-11 23:24:11 -0400

    adding folders FlourPackProblem, LargestPrime, AllFactors, DiagonalStar, PatternPractice, PatternPracticeTriangle

--- LargestPrime/src/LargestPrime.java
+++ LargestPrime/src/LargestPrime.java
@@ -1,8 +1,26 @@
 public class LargestPrime {
     public static int getLargestPrime(int number) {
-        if (number <= 1) {
-            return -1;
+
+        if (number <= 1) return -1;
+
+        int largestPrime = 0;
+
+        while (number % 2 == 0) {
+            largestPrime = 2;
+            number /= 2;
         }
 
+        for (int i = 3; i <= Math.sqrt(number); i += 2) {
+
+            while (number % i == 0) {
+                largestPrime = i;
+                number /= i;
+            }
+        }
+
+        if (number > 2) {
+            largestPrime = number;
+        }
+        return largestPrime;
     }
 }
--- LargestPrime/src/Main.java
+++ LargestPrime/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
+        LargestPrime.getLargestPrime(21);
+        LargestPrime.getLargestPrime(217);
+        LargestPrime.getLargestPrime(0);
+        LargestPrime.getLargestPrime(45);
+        LargestPrime.getLargestPrime(-1);
     }
 }
