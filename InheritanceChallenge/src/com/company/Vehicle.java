e0d392f597d87dbfefd392690cfbdc949399cdbc
lindsive <ivesonlindsay@gmail.com>
2020-08-16 23:29:25 -0400

    made edits to InheritanceChallenge and created project Cylinder

--- InheritanceChallenge/src/com/company/Vehicle.java
+++ InheritanceChallenge/src/com/company/Vehicle.java
@@ -2,11 +2,48 @@ package com.company;
 
 public class Vehicle {
 
-    private String steering;
-    private String changeGear;
-    private int moving;
+    private String name;
+    private String size;
 
-    public Vehicle(String steering, String changeGear, int moving) {
-        this(String steering, );
+    private int currentVelocity;
+    private int currentDirection;
+
+    public Vehicle(String name, String size) {
+        this.name = name;
+        this.size = size;
+
+        this.currentVelocity = 0;
+        this.currentDirection = 0;
+    }
+
+    public void steer(int direction) {
+        this.currentDirection += direction;
+        System.out.println("Vehicles.steer(): Steerting at " + currentDirection + " degrees");
+    }
+
+    public void move(int velocity, int direction) {
+        currentVelocity = velocity;
+        currentDirection = direction;
+        System.out.println("Vehicle.move(): Moving at " + currentVelocity + " in direction " + currentDirection);
+    }
+
+    public void stop() {
+        this.currentVelocity = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public int getCurrentVelocity() {
+        return currentVelocity;
+    }
+
+    public int getCurrentDirection() {
+        return currentDirection;
     }
 }
